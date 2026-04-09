52c9207c265ead4a0f428bfd53c6ff7081186bc6
LnMeet <LnMeet@users.noreply.github.com>
2026-04-09 07:59:04 +0000

    feat: Java 登录入口实现默认访问
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/main/java/com/login/LoginServlet.java
+++ src/main/java/com/login/LoginServlet.java
@@ -1,10 +1,10 @@
 package com.login;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet {
     
